d9a43e059e15660dfaf936bba778c7b44c296bb9
Arjen Swellengrebel <arjen_swellengrebel@msn.com>
2014-04-23 15:58:23 +0200

    Close
    Don't forget to add kdtree to the matlab path

--- ICP.m
+++ ICP.m
@@ -3,37 +3,29 @@ function output = ICP( A1, A2, sampleSize, sampleTech )
     A1 = removeNoise(A1); %base
     A2 = removeNoise(A2); %target
     disp('A1');
-    size(A1)
     A1 = subsampling(A1, sampleSize, sampleTech);
     disp('A2');
-    size(A2)
     A2 = subsampling(A2, sampleSize, sampleTech);
 
-    % compute distances
-    disp('calculating distances...');
-    dd = distances(A1, A2);
-
-    disp('calculating the point in the target set that is nearest to each of the base set...');
-    [mindist target_index] = min(dd,[],2);
-
-    disp('all done.');
-    output = [mindist target_index];
-end
-
-function ret = flatten(in)
-    n_rows = size(in,1);
-    n_cols = size(in,2);
-    n_cells = n_rows * n_cols;
-    ret = zeros(n_cells, 3);
-    i = 0;
-    for r = 1:n_rows
-        for c = 1:n_cols
-            i = i + 1;
-            ret(i,1) = r;
-            ret(i,2) = c;
-            ret(i,3) = in(r,c);
-        end
+    tree = kd_buildtree(A2,0);
+    vec_value_list = zeros(sampleSize,4);
+    for i = 1:sampleSize
+        point = A1(i,:);
+        [index_vals,vec_vals,node_number] = kd_closestpointfast(tree,point);
+        vec_value_list(i,:) = vec_vals;
     end
+    monstrosity = [A1';vec_value_list'];
+    SVD(monstrosity)
+    
+    % compute distances
+    %disp('calculating distances...');
+    %dd = distances(A1, A2);
+
+    %disp('calculating the point in the target set that is nearest to each of the base set...');
+    %[mindist target_index] = min(dd,[],2);
+
+    %disp('all done.');
+    %output = [mindist target_index];
 end
 
 function out = subsampling(in, sampleSize, technique)
