ef63fc7a361fb17570d8cd28b7f099acf9d75e73
Arjen Swellengrebel <arjen_swellengrebel@msn.com>
2014-04-23 16:51:53 +0200

    Need to update R?

--- ICP.m
+++ ICP.m
@@ -2,30 +2,28 @@ function output = ICP( A1, A2, sampleSize, sampleTech )
     R = eye(3);
     A1 = removeNoise(A1); %base
     A2 = removeNoise(A2); %target
-    disp('A1');
-    A1 = subsampling(A1, sampleSize, sampleTech);
-    disp('A2');
-    A2 = subsampling(A2, sampleSize, sampleTech);
+    A1 = A1(:,1:3);
+    A2 = A2(:,1:3);
+    baseCloud = subsampling(A1, sampleSize, sampleTech);
+    targetCloud = subsampling(A2, sampleSize, sampleTech);
 
-    tree = kd_buildtree(A2,0);
-    vec_value_list = zeros(sampleSize,4);
+    tree = kd_buildtree(targetCloud,0);
+    vec_value_list = zeros(sampleSize,3);
     for i = 1:sampleSize
-        point = A1(i,:);
+        point = baseCloud(i,:);
         [index_vals,vec_vals,node_number] = kd_closestpointfast(tree,point);
         vec_value_list(i,:) = vec_vals;
     end
-    monstrosity = [A1';vec_value_list'];
-    SVD(monstrosity)
-    
-    % compute distances
-    %disp('calculating distances...');
-    %dd = distances(A1, A2);
-
-    %disp('calculating the point in the target set that is nearest to each of the base set...');
-    %[mindist target_index] = min(dd,[],2);
-
-    %disp('all done.');
-    %output = [mindist target_index];
+    monstrosity = [baseCloud';vec_value_list'];
+    baseCloudCenter = mean(baseCloud,1)
+    targetCloudCenter = mean(vec_value_list,1)
+    [U,S,V] = svd(monstrosity);
+    Vt = V';
+    U = U(:,1:3)
+    S = S(1:3,1:3)
+    Vt = Vt(1:3,:)
+    R
+    R = Vt*R'*U;
 end
 
 function out = subsampling(in, sampleSize, technique)
