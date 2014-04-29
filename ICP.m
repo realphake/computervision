5be5678a8c0b546d1862c3f54a6d3a35d5c899c2
beamcannon <beamcannon@gmail.com>
2014-04-29 14:56:11 +0200

    FIXED!

--- ICP.m
+++ ICP.m
@@ -1,6 +1,8 @@
 function [T_total, R_total] = ICP( base, target, sampleSize, sampleTech )
     R = eye(3);
+    R_total = eye(3);
     T = [0, 0, 0];
+    T_total = [0, 0, 0];
     base = removeNoise(base); %base
     target = removeNoise(target); %target
     base = base(:,1:3);
@@ -13,22 +15,28 @@ function [T_total, R_total] = ICP( base, target, sampleSize, sampleTech )
     % targetCloud = subsampling(target, sampleSize, sampleTech);
     IDX = knnsearch(target,baseCloud, 'NSMethod','kdtree');
     targetCloud = target(IDX,:);
-    size(targetCloud);
     
     baseCloudCenter = mean(baseCloud,1);
     targetCloudCenter = mean(targetCloud,1);
+    figure(1);
+    scatter3(baseCloud(:,1),baseCloud(:,2),baseCloud(:,3), 3, [1, 0, 0]);
+    hold on
+    scatter3(targetCloud(:,1),targetCloud(:,2),targetCloud(:,3), 3, [0, 0.5, 0]);
+    hold off
+    pause(3);
     A = (baseCloud - repmat(baseCloudCenter, sampleSize,1))' * (targetCloud - repmat(targetCloudCenter, sampleSize,1));
     [U,S,V] = svd(A);
-    R_total = U*R*V';
-    R = U*V'
-    T_total = T + ( baseCloudCenter - targetCloudCenter * R );
-    T = baseCloudCenter - targetCloudCenter * R
+    R_total = R_total*(U*V');
+    R = U*V';
+    T = baseCloudCenter - (targetCloudCenter * R');
+    T_total = T_total + T;
     targetCloud = (R * targetCloud')' + repmat(T,sampleSize,1);
+    old_error = -1;
     error = norm(mean(baseCloud-targetCloud,1))
     pause on
-    
-    while error > 0.0012
-        target_new = (R * target')' + repmat(T,length(target),1);
+    target_new = target;
+    while error ~= old_error
+        target_new = (R * target_new')' + repmat(T,length(target),1);
         figure(1);
         scatter3(baseCloud(:,1),baseCloud(:,2),baseCloud(:,3), 3, [1, 0, 0]);
         hold on
@@ -38,19 +46,20 @@ function [T_total, R_total] = ICP( base, target, sampleSize, sampleTech )
         
         baseCloud = subsampling(base, sampleSize, sampleTech);
         IDX = knnsearch(target_new,baseCloud, 'NSMethod','kdtree');
-        targetCloud = target(IDX,:);
-        size(targetCloud);
+        targetCloud = target_new(IDX,:);
         
         baseCloudCenter = mean(baseCloud,1);
         targetCloudCenter = mean(targetCloud,1);
         A = (baseCloud - repmat(baseCloudCenter, sampleSize,1))' * (targetCloud - repmat(targetCloudCenter, sampleSize,1));
         [U,S,V] = svd(A);
-        R_total = U*R*V'
+        R_total = R_total*(U*V')
         R = U*V';
-        T_total = T + ( baseCloudCenter - targetCloudCenter * R )
-        T = baseCloudCenter - targetCloudCenter * R;
+        T = baseCloudCenter - (targetCloudCenter * R');
+        T_total = T_total + T
         targetCloud = (R * targetCloud')' + repmat(T,sampleSize,1);
+        old_error = error;
         error = norm(mean(baseCloud-targetCloud,1))
+        [x, y, z] = decompose_rotation(R_total)
     end
     pause off
 end
@@ -71,4 +80,10 @@ end
 
 function noNoise = removeNoise(noise)
     noNoise = noise(noise(:,3) < 2,:);
+end
+
+function [x,y,z] = decompose_rotation(R)
+	x = atan2(R(3,2), R(3,3));
+	y = atan2(-R(3,1), sqrt(R(3,2)*R(3,2) + R(3,3)*R(3,3)));
+	z = atan2(R(2,1), R(1,1));
 end
