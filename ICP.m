ac0dd53ec59cf13bf98063f9b719d88fd4f92ea6
stablum@gmail.com <stablum@gmail.com>
2014-04-29 12:20:21 +0200

    variable renaming

--- ICP.m
+++ ICP.m
@@ -1,62 +1,66 @@
-function [T_total R_total] = ICP( A1, A2, sampleSize, sampleTech )
+function [T_total R_total] = ICP( source, target, sampleSize, sampleTech )
     R = eye(3);
     T = [0, 0, 0];
-    A1 = removeNoise(A1); %base
-    A2 = removeNoise(A2); %target
-    A1 = A1(:,1:3);
-    A2 = A2(:,1:3);
+    source = removeNoise(source); % will be sampled and transformed
+    target = removeNoise(target); % will not be transformed or sample
     
-    baseCloud = subsampling(A1, sampleSize, sampleTech);
-    targetCloud = subsampling(A2, sampleSize, sampleTech);
+    % selecting the first three columns that have the actual x y z
+    % coordinates
+    source = source(:,1:3);
+    target = target(:,1:3);
+    
+    sourceCloud = subsampling(source, sampleSize, sampleTech);
+    targetCloud = target; % target is not sampled: the search is done in all its points
+    %targetCloud = subsampling(target, sampleSize, sampleTech);
 
     tree = kd_buildtree(targetCloud,0);
     vec_value_list = zeros(sampleSize,3);
     for i = 1:sampleSize
-        point = baseCloud(i,:);
+        point = sourceCloud(i,:);
         [index_vals,vec_vals,node_number] = kd_closestpointfast(tree,point);
         vec_value_list(i,:) = vec_vals;
     end
 
-    baseCloudCenter = mean(baseCloud,1);
+    sourceCloudCenter = mean(sourceCloud,1);
     targetCloudCenter = mean(vec_value_list,1);
-    A = (baseCloud - repmat(baseCloudCenter, sampleSize,1))' * (vec_value_list - repmat(targetCloudCenter, sampleSize,1));
+    A = (sourceCloud - repmat(sourceCloudCenter, sampleSize,1))' * (vec_value_list - repmat(targetCloudCenter, sampleSize,1));
     [U,S,V] = svd(A);
     R_total = U*R*V';
     R = U*V'
-    T_total = T + ( baseCloudCenter - targetCloudCenter * R );
-    T = baseCloudCenter - targetCloudCenter * R
+    T_total = T + ( sourceCloudCenter - targetCloudCenter * R );
+    T = sourceCloudCenter - targetCloudCenter * R
     targetCloud = (R * targetCloud')' + repmat(T,sampleSize,1);
-    A2_new = zeros(size(A2));
-    A2_center = mean(A2,1);
-    error = norm(mean(baseCloud-targetCloud,1));
+    target_new = zeros(size(target));
+    target_center = mean(target,1);
+    error = norm(mean(sourceCloud-targetCloud,1));
     pause on
     while error > 0.0012
-        A2_new = (R * A2')' + repmat(T,length(A2),1);
+        target_new = (R * target')' + repmat(T,length(target),1);
         figure(1);
-        scatter3(baseCloud(:,1),baseCloud(:,2),baseCloud(:,3), 3, [1, 0, 0]);
+        scatter3(sourceCloud(:,1),sourceCloud(:,2),sourceCloud(:,3), 3, [1, 0, 0]);
         scatter3(targetCloud(:,1),targetCloud(:,2),targetCloud(:,3), 3, [0, 0.5, 0]);
         drawnow();
-        baseCloud = subsampling(A1, sampleSize, sampleTech);
-        targetCloud = subsampling(A2_new, sampleSize, sampleTech);
+        sourceCloud = subsampling(source, sampleSize, sampleTech);
+        targetCloud = subsampling(target_new, sampleSize, sampleTech);
 
         tree = kd_buildtree(targetCloud,0);
         vec_value_list = zeros(sampleSize,3);
         for i = 1:sampleSize
-            point = baseCloud(i,:);
+            point = sourceCloud(i,:);
             [index_vals,vec_vals,node_number] = kd_closestpointfast(tree,point);
             vec_value_list(i,:) = vec_vals;
         end
 
-        baseCloudCenter = mean(baseCloud,1);
+        sourceCloudCenter = mean(sourceCloud,1);
         targetCloudCenter = mean(vec_value_list,1);
-        A = (baseCloud - repmat(baseCloudCenter, sampleSize,1))' * (vec_value_list - repmat(targetCloudCenter, sampleSize,1));
+        A = (sourceCloud - repmat(sourceCloudCenter, sampleSize,1))' * (vec_value_list - repmat(targetCloudCenter, sampleSize,1));
         [U,S,V] = svd(A);
         R_total = U*R*V'
         R = U*V';
-        T_total = T + ( baseCloudCenter - targetCloudCenter * R )
-        T = baseCloudCenter - targetCloudCenter * R;
+        T_total = T + ( sourceCloudCenter - targetCloudCenter * R )
+        T = sourceCloudCenter - targetCloudCenter * R;
         targetCloud = (R * targetCloud')' + repmat(T,sampleSize,1);
-        error = norm(mean(baseCloud-targetCloud,1))
+        error = norm(mean(sourceCloud-targetCloud,1))
     end
     pause off
 end
