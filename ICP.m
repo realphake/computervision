fbaaadad6f4ea1410d8a2aa1049be9fa8def0b97
beamcannon <beamcannon@gmail.com>
2014-04-30 15:44:35 +0200

    changes made for speed

--- ICP.m
+++ ICP.m
@@ -3,10 +3,7 @@ function [T_total, R_total] = ICP( base, target, sampleSize, sampleTech )
     R_total = eye(3);
     T = [0, 0, 0];
     T_total = [0, 0, 0];
-    base = removeNoise(base); %base
-    target = removeNoise(target); %target
-    base = base(:,1:3);
-    target = target(:,1:3);
+
     
     % selecting the first three columns that have the actual x y z
     % coordinates
@@ -18,12 +15,14 @@ function [T_total, R_total] = ICP( base, target, sampleSize, sampleTech )
     
     baseCloudCenter = mean(baseCloud,1);
     targetCloudCenter = mean(targetCloud,1);
-    figure(1);
-    scatter3(baseCloud(:,1),baseCloud(:,2),baseCloud(:,3), 3, [1, 0, 0]);
-    hold on
-    scatter3(targetCloud(:,1),targetCloud(:,2),targetCloud(:,3), 3, [0, 0.5, 0]);
-    hold off
-    pause(3);
+    
+%   figure(1);
+%   scatter3(baseCloud(:,1),baseCloud(:,2),baseCloud(:,3), 3, [1, 0, 0]);
+%   hold on
+%   scatter3(targetCloud(:,1),targetCloud(:,2),targetCloud(:,3), 3, [0, 0.5, 0]);
+%   hold off
+%   pause(3);
+
     A = (baseCloud - repmat(baseCloudCenter, sampleSize,1))' * (targetCloud - repmat(targetCloudCenter, sampleSize,1));
     [U,S,V] = svd(A);
     R_total = R_total*(U*V');
@@ -32,17 +31,12 @@ function [T_total, R_total] = ICP( base, target, sampleSize, sampleTech )
     T_total = T_total + T;
     targetCloud = (R * targetCloud')' + repmat(T,sampleSize,1);
     old_error = -1;
-    error = norm(mean(baseCloud-targetCloud,1))
+    error = norm(mean(baseCloud-targetCloud,1));
     pause on
     target_new = target;
-    while error ~= old_error
+    iterations = 0;
+    while error ~= old_error && iterations ~= 50 
         target_new = (R * target_new')' + repmat(T,length(target),1);
-        figure(1);
-        scatter3(baseCloud(:,1),baseCloud(:,2),baseCloud(:,3), 3, [1, 0, 0]);
-        hold on
-        scatter3(targetCloud(:,1),targetCloud(:,2),targetCloud(:,3), 3, [0, 0.5, 0]);
-        hold off
-        pause(3);
         
         baseCloud = subsampling(base, sampleSize, sampleTech);
         IDX = knnsearch(target_new,baseCloud, 'NSMethod','kdtree');
@@ -52,15 +46,23 @@ function [T_total, R_total] = ICP( base, target, sampleSize, sampleTech )
         targetCloudCenter = mean(targetCloud,1);
         A = (baseCloud - repmat(baseCloudCenter, sampleSize,1))' * (targetCloud - repmat(targetCloudCenter, sampleSize,1));
         [U,S,V] = svd(A);
-        R_total = R_total*(U*V')
+        R_total = R_total*(U*V');
         R = U*V';
         T = baseCloudCenter - (targetCloudCenter * R');
-        T_total = T_total + T
+        T_total = T_total + T;
         targetCloud = (R * targetCloud')' + repmat(T,sampleSize,1);
         old_error = error;
-        error = norm(mean(baseCloud-targetCloud,1))
-        [x, y, z] = decompose_rotation(R_total)
+        error = norm(mean(baseCloud-targetCloud,1));
+%         [x, y, z] = decompose_rotation(R_total);
+        iterations = iterations + 1;
     end
+%     figure(1);
+%     showResultBase = subsampling(base, 3000, 'uniform');
+%     showResultTarget = subsampling(target_new, 3000, 'uniform');
+%     scatter3(showResultBase(:,1),showResultBase(:,2),showResultBase(:,3), 3, [1, 0, 0]);
+%     hold on
+%     scatter3(showResultTarget(:,1),showResultTarget(:,2),showResultTarget(:,3), 3, [0, 0.5, 0]);
+%     hold off
     pause off
 end
 
@@ -78,12 +80,13 @@ function uniform = uniformSubsampling(data, samples)
     uniform = data( 1:stepsize:size(data,1), : );
 end
 
-function noNoise = removeNoise(noise)
-    noNoise = noise(noise(:,3) < 2,:);
-end
-
+% decompose copied over from http://nghiaho.com/uploads/code/rotation_matrix_demo.m
 function [x,y,z] = decompose_rotation(R)
 	x = atan2(R(3,2), R(3,3));
 	y = atan2(-R(3,1), sqrt(R(3,2)*R(3,2) + R(3,3)*R(3,3)));
 	z = atan2(R(2,1), R(1,1));
 end
+
+% function noNoise = removeNoise(noise)
+%     noNoise = noise(noise(:,3) < 2,:);
+% end
