6c450705890afea3bf0e31e463b71cdef5ea7077
Arjen Swellengrebel <arjen_swellengrebel@msn.com>
2014-05-23 17:11:56 +0200

    Done, but has a bug.
    Fairly certain the bug is caused by the filling of pointViewMatrix; it should be filled from the left, not using the indices.

--- ChainFME.m
+++ ChainFME.m
@@ -1,18 +1,20 @@
-function pointViewMatrix = ChainFME( )
+function ChainFME( )
 
-    [~,matches] = FME(sprintf('obj02_%03d.png',16), sprintf('obj02_%03d.png',1), '');
-    pointViewMatrix = zeros( 1, matches(1,length(matches)) );
-    pointViewMatrix(matches(1,:)) = matches(2,:);
+    [~,matches,f1,f2] = FME(sprintf('obj02_%03d.png',16), sprintf('obj02_%03d.png',1), '');
+    pointViewMatrix = zeros( 2, matches(1,length(matches)) );
+    pointViewMatrix(1,matches(1,:)) = f2 ( 1, matches(2,:) ) ;
+    pointViewMatrix(2,matches(1,:)) = f2 ( 2, matches(2,:) ) ;
     
     for i = 1:15
-        [~,matches] = FME(sprintf('obj02_%03d.png',i), sprintf('obj02_%03d.png',i+1), '');
+        [~,matches,f1,f2] = FME(sprintf('obj02_%03d.png',i), sprintf('obj02_%03d.png',i+1), '');
         if matches(1,length(matches)) > length(pointViewMatrix)
-            temp = zeros( i+1, matches(1,length(matches)) );
+            temp = zeros( i*2+2, matches(1,length(matches)) );
         else
-            temp = zeros( i+1, length(pointViewMatrix) );
+            temp = zeros( i*2+2, length(pointViewMatrix) );
         end
         temp(1:size(pointViewMatrix, 1), 1:size(pointViewMatrix, 2)) = pointViewMatrix;
-        temp(i+1, matches(1,:)) = matches(2,:);
+        temp(i*2+1, matches(1,:)) = f2 ( 1, matches(2,:) ) ;
+        temp(i*2+2, matches(1,:)) = f2 ( 2, matches(2,:) ) ;
         pointViewMatrix = temp;
     end
     
@@ -25,6 +27,8 @@ function pointViewMatrix = ChainFME( )
     S = (W3.^0.5) * (V3');
     % OR:
     % M = U3
-    % S = W3* (V3')
+    % S = W3* (V3') 
+    
+    fscatter3( S(1,:), S(2,:), S(3,:), [0,10] );
     
 end
