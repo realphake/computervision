ee8dd7521c6ee5dc8e6dbc48fc262accd3027fc6
Arjen Swellengrebel <arjen_swellengrebel@msn.com>
2014-05-27 23:09:11 +0200

    No idea if this works.

--- ChainFME.m
+++ ChainFME.m
@@ -1,21 +1,24 @@
 function ChainFME( )
 
     [~,matches,f1,f2] = FME(sprintf('obj02_%03d.png',16), sprintf('obj02_%03d.png',1), '');
-    pointViewMatrix = zeros( 2, matches(1,length(matches)) );
-    pointViewMatrix(1,matches(1,:)) = f2 ( 1, matches(2,:) ) ;
-    pointViewMatrix(2,matches(1,:)) = f2 ( 2, matches(2,:) ) ;
+	pointViewMatrix = matches;
     
     for i = 1:15
         [~,matches,f1,f2] = FME(sprintf('obj02_%03d.png',i), sprintf('obj02_%03d.png',i+1), '');
-        if matches(1,length(matches)) > length(pointViewMatrix)
-            temp = zeros( i*2+2, matches(1,length(matches)) );
-        else
-            temp = zeros( i*2+2, length(pointViewMatrix) );
-        end
-        temp(1:size(pointViewMatrix, 1), 1:size(pointViewMatrix, 2)) = pointViewMatrix;
-        temp(i*2+1, matches(1,:)) = f2 ( 1, matches(2,:) ) ;
-        temp(i*2+2, matches(1,:)) = f2 ( 2, matches(2,:) ) ;
-        pointViewMatrix = temp;
+		currentLength = length (pointViewMatrix) ;
+		pointViewMatrix = [pointViewMatrix;zeros(size(pointViewMatrix,1))];
+		for j = 1:length (matches)
+			[contains,column] = ismemberf (pointViewMatrix(end, :), matches(1, i));
+			if ~contains
+				column = currentLength+1;
+				currentLength += 1;
+				pointViewMatrix = [pointViewMatrix,zeros(size(pointViewMatrix,2))];
+				pointViewMatrix(end, column) = matches(2,j);
+			else
+				pointViewMatrix(end, column) = matches(2,j);
+			end
+			
+		end
     end
     
     [U, W, V] = svd(pointViewMatrix);
