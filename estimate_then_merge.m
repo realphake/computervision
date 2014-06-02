85ee6660d5db6a92d7b61b76cf6ef8bb99ded6b1
Norbert Heijne <norbert.heijne@gmail.com>
2014-06-02 18:53:02 +0200

    Added comments, fixed merge then estimate

--- estimate_then_merge.m
+++ estimate_then_merge.m
@@ -1,4 +1,14 @@
-function [ out_pointcloud, R, T ] = estimate_then_merge(useTheseNumbers, sampleSize, sampleTech)
+function [ out_pointcloud, R, T ] = estimate_then_merge(useTheseNumbers, sampleSize, sampleTech, maxIterations)
+    % ** Preferably use merge_pointclouds as the main function **
+    % useTheseNumbers: a list of image numbers to use example: [0:1:99]
+    % sampleSize: total amount of points sampled, except for normal space,
+    % which indicates the samples per bin
+    % sampleTech: 'none', 'uniform', 'random', 'normals'
+    % maxIterations: set to 0 to not use the maximum iterations, otherwise
+    % the ICP algorithm will only use the maxIterations for merging the two
+    % pointclouds
+    
+    % initialize output variables
     T = zeros(1, 3, length(useTheseNumbers));
     R = zeros(3, 3, length(useTheseNumbers));
     R(:, :, 1) = eye(3);
@@ -6,7 +16,10 @@ function [ out_pointcloud, R, T ] = estimate_then_merge(useTheseNumbers, sampleS
     last_R = eye(3);
     cutoffpoint_noise = 2;
     Base = readPcd(['00000000', sprintf('%02d', useTheseNumbers(1)), '.pcd']);
+    % remove noise from the read point cloud
     if ( strcmpi( sampleTech, 'normals' ) )
+        % if the technique involves normal space, then read in the normals
+        % as well and remove the noise that is located in both data sets
         normals = readPcd(['00000000', sprintf('%02d', useTheseNumbers(1)), '_normal.pcd']);
         [ Base, normals ] = removeNoise(Base, cutoffpoint_noise, normals); 
     else
@@ -14,19 +27,27 @@ function [ out_pointcloud, R, T ] = estimate_then_merge(useTheseNumbers, sampleS
     end
     Base = removeNoise(Base, cutoffpoint_noise); 
     out_pointcloud = Base;
+    
     merges_done = 1;
     for i = useTheseNumbers(2:length(useTheseNumbers))
-        disp(num2str(i));
+        disp('Opening pointcloud ', num2str(i));
+        % read in the target point cloud
         Target = readPcd(['00000000', sprintf('%02d', i), '.pcd']);
         if ( strcmpi( sampleTech, 'normals' ) )
+            % if the technique involves normal space, then read in the normals
+            % as well and remove the noise that is located in both data sets
             next_normals = readPcd(['00000000', sprintf('%02d', i), '_normal.pcd']);
-            [ Target, next_normals ] = removeNoise(Target, cutoffpoint_noise, next_normals); %target
-            [output, R_obtained, T_obtained] = ICP(Base, Target, sampleSize, sampleTech, normals);
+            [ Target, next_normals ] = removeNoise(Target, cutoffpoint_noise, next_normals);
+            % get the rotation and translation of the base from ICP with
+            % while using normal space uniform sampling
+            [output, R_obtained, T_obtained] = ICP(Base, Target, sampleSize, sampleTech, maxIterations, normals);
             normals = next_normals;
         else
+            % get the rotation and translation of the base from ICP
             Target = removeNoise(Target, cutoffpoint_noise); %target
-            [output, R_obtained, T_obtained] = ICP(Base, Target, sampleSize, sampleTech);
+            [output, R_obtained, T_obtained] = ICP(Base, Target, sampleSize, sampleTech, maxIterations);
         end
+        % update the output variables
         out_pointcloud = [out_pointcloud;(last_R * output')' + repmat(last_T,length(output),1)];
         last_T = last_T + T_obtained * last_R';
         last_R = last_R * R_obtained;
